de64be93719e88ad3543352fe23337895e6921a4
Scot Sexton <rss3509@gmail.com>
2020-04-06 23:38:43 +0000

    Automatically backed up by Learn

--- play.rb
+++ play.rb
@@ -1,5 +1,13 @@
-count = 0
+=count = 0
 while count < 6 do
   puts "say this forever..."
   count = count +=1
+end
+
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
 end
